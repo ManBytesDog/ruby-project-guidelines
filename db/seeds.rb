f9f2aa14c965a960d51505d484eabcc0a31f8afe
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2020-12-02 11:16:05 -0500

    Updated seed file

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+User.destroy_all 
+
+user1 = User.create(name: "Shredder")
+user2 = User.create(name: "Blues Man")
+user3 = User.create(name: "Jazz Man")
+user4 = User.create(name: "Cool Fool")
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts "Seed file ran."
