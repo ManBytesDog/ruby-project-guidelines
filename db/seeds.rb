35c7117fc1b1c3065447498bf9c5080703b35211
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2020-12-02 13:03:46 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-User.destroy_all 
+User.destroy_all
+Guitar.destroy_all 
 
 user1 = User.create(name: "Shredder")
 user2 = User.create(name: "Blues Man")
@@ -7,6 +8,11 @@ user4 = User.create(name: "Cool Fool")
 
 
 
+gtr1 = Guitar.create(user_id: user1.id, manufacturer: "Fender", model: "Strat")
+gtr2 = Guitar.create(user_id: user1.id, manufacturer: "Gibson", model: "Les Paul")
+gtr3 = Guitar.create(user_id: user3.id, manufacturer: "Fender", model: "Tele")
+gtr4 = Guitar.create(user_id: user2.id, manufacturer: "Martin", model: "Dreadnaught")
+gtr5 = Guitar.create(user_id: user4.id, manufacturer: "Gibson", model: "Les Paul")
 
 
 
