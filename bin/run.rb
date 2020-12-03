dd36347be3dd589d1468fb34b191fad0607d1ff0
logicfirst <slee@logicfirstllc.com>
2020-12-03 10:52:12 -0500

    update to environment.rb and run.rb, added Welcome

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,25 @@
 require_relative '../config/environment'
 require "tty-prompt"
+require 'pry'
+# require 'colorize'
 
 prompt = TTY::Prompt.new
 
-prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
+system "clear"
 
+puts "🎸🎸🎸Welcome back to Guitar Parts Warehouse!🎸🎸🎸"
+puts " "
+puts " "
+uname = prompt.ask("What is your user name?", default: "Mad Guitar Player")
 
+  if User.find_by(name: uname)
+    uname = User.find_by(name: uname)
+    selected_user = uname.find_by(name: uname)
+    prompt.select("Hey #{selected_user} which guitar would you like to order parts for today?", %w("Fender"))
+  end
 
-puts "HELLO WORLD"
+puts ""
+puts ""
+puts ""
+puts ""
+puts "It's Working!!!!!"
