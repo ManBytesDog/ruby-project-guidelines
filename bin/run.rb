8dd7721cb8fa8beabd4f0bed83394a9226ff6bc0
logicfirst <slee@logicfirstllc.com>
2020-12-03 13:00:26 -0500

    prompt for guitar manufacturer working

--- bin/run.rb
+++ bin/run.rb
@@ -13,10 +13,18 @@ puts " "
 uname = prompt.ask("What is your user name?", default: "Mad Guitar Player")
 
   if User.find_by(name: uname)
-    uname = User.find_by(name: uname)
-    selected_user = uname.find_by(name: uname)
-    prompt.select("Hey #{selected_user} which guitar would you like to order parts for today?", %w("Fender"))
+    selected_user = User.find_by(name: uname) 
   end
+  puts " "
+    user_guitars_manu = selected_user.guitars.map { |guitar| guitar.manufacturer}
+    manufacturer_list = prompt.select("Hey #{selected_user.name} which guitar would you like to order parts for today?", user_guitars_manu)
+    puts " "
+    if user_guitars_manu = "Fender"
+    #  prompt.select "Nice Guitars"
+      user_guitars_model = selected_user.guitars.map { |guitar| guitar.model} 
+      model_select = prompt.select("Nice Guitars! Which model?", user_guitars_model)
+    end
+  
 
 puts ""
 puts ""
