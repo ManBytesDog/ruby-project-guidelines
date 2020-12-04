b78b9e7f797dda546f21662c6cb07838fd677a88
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2020-12-03 21:10:08 -0500

    Added puts to run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -26,7 +26,8 @@ uname = prompt.ask("What is your user name?", default: "Mad Guitar Player")
 
       puts " "
       puts "Lets get something for your #{user_manufacturer}!"
-      #binding.pry
+      puts " "
+      
 
 
 
