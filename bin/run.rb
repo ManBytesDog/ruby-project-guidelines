6b38bc162b3b2274268777007f029a9ba5ed1b9a
logicfirst <slee@logicfirstllc.com>
2020-12-04 08:01:51 -0500

    Guitar linked to Part

--- bin/run.rb
+++ bin/run.rb
@@ -15,10 +15,10 @@ uname = prompt.ask("What is your user name?", default: "Mad Guitar Player")
     selected_user = User.find_by(name: uname) 
   end
   puts " "
-    user_guitars_manu = selected_user.guitars.map { |guitar| guitar.manufacturer}
-    manufacturer_list = prompt.select("Hey #{selected_user.name} which guitar would you like to order parts for today?", user_guitars_manu)
+    user_guitars_manufacturer = selected_user.guitars.map { |guitar| guitar.manufacturer}
+    manufacturer_list = prompt.select("Hey #{selected_user.name} which guitar would you like to order parts for today?", user_guitars_manufacturer)
     puts " "
-    if user_guitars_manu 
+    if user_guitars_manufacturer 
       guitar_models = selected_user.guitars.select { |guitar| guitar.manufacturer == manufacturer_list}.map { |guitar| guitar.model}
       model_select = prompt.select("Nice Guitars! Which model?", guitar_models)
     end
@@ -27,10 +27,15 @@ uname = prompt.ask("What is your user name?", default: "Mad Guitar Player")
       puts " "
       puts "Lets get something for your #{user_manufacturer}!"
       puts " "
-      
 
-
-
+        if Guitar.find_by(manufacturer: user_manufacturer)
+          selected_guitar = Guitar.find_by(manufacturer: user_manufacturer)
+          selected_guitar.parts.each do |part|
+            puts part.part_name, part.color, part.material, part.description, part.price
+          end
+        end
+     
+    
   
 
 puts ""
